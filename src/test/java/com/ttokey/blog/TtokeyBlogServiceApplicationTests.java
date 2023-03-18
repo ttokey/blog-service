ff28457ddae7b3450a19a57c10bc59a1750d0f51
ttokey <ttokey.lee@gmail.com>
2023-03-18 11:12:23 +0900

    feat : init project

---
+++ src/test/java/com/ttokey/blog/TtokeyBlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttokey.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtokeyBlogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
